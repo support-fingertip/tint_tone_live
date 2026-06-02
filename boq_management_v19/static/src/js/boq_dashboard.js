3a94e9ba15e4af61778a9b287b3c2a2670118b0c
K.Praveen <k.praveen@fingertipplus.com>
2026-06-02 16:53:52 +0530

    Header Supply Chain Name Changes

--- boq_management_v19/static/src/js/boq_dashboard.js
+++ boq_management_v19/static/src/js/boq_dashboard.js
@@ -437,7 +437,7 @@ export class HeadSupplierDashboard extends BoqManagerDashboardBase {
     static template       = "boq_management_v19.HeadSupplierDashboard";
 
     get isHeadDashboard()   { return true; }
-    get dashboardTitle()    { return "Head of Supplier Dashboard"; }
+    get dashboardTitle()    { return "Head of Supply Chain"; }
     get dashboardSubtitle() { return "Consolidated multi-company supplier & procurement view"; }
     get dashboardIcon()     { return "fa-globe"; }
     get partnerLabel()      { return "Partner"; }
