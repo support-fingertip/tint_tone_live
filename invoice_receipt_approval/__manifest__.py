fe92b986999fcdd46b9736999d0bf0355733cd59
Arjun <a4arjunsatheesh@gmail.com>
2026-06-03 16:02:22 +0530

    [UPDT]Removed Connect your software option in RFQ report

--- invoice_receipt_approval/__manifest__.py
+++ invoice_receipt_approval/__manifest__.py
@@ -14,6 +14,7 @@
         'security/ir.model.access.csv',
         'views/invoice_reject_wizard_views.xml',
         'views/res_config_settings_views.xml',
+        'views/purchase_quotation_templates.xml',
         'views/account_move_views.xml',
         'views/hr_expense_views.xml',
     ],
