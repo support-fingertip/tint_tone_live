6406d7552ad74a1483f7604e6405c56438184d97
Arjun <a4arjunsatheesh@gmail.com>
2026-06-04 18:11:12 +0530

    [ADD]Initial commit for the module tts_purchase_boq_validations

---
+++ tts_purchase_boq_validations/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'TTS Purchase and BOQ Validations',
+    'version': '1.0',
+    'category': 'Customizations',
+    'summary': 'Validations for purchase.order and boq.boq',
+    'description': """
+This module provides the necessary business validations for Purchase Orders and Bill of Quantities.
+    """,
+    'depends': ['purchase', 'boq_management_v19'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ tts_purchase_boq_validations/models/purchase_order.py
@@ -0,0 +1,47 @@
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        today = fields.Date.context_today(self)
+        for vals in vals_list:
+            if vals.get('date_order'):
+                date_order = fields.Datetime.to_datetime(vals['date_order']).date()
+                if date_order < today:
+                    raise ValidationError(_("Order Date cannot be set to a past date."))
+            if vals.get('validity_end_date'):
+                validity_date = fields.Date.to_date(vals['validity_end_date'])
+                if validity_date < today:
+                    raise ValidationError(_("Validity End Date cannot be set to a past date."))
+        return super().create(vals_list)
+
+    def write(self, vals):
+        today = fields.Date.context_today(self)
+        if vals.get('date_order'):
+            date_order = fields.Datetime.to_datetime(vals['date_order']).date()
+            if date_order < today:
+                raise ValidationError(_("Order Date cannot be set to a past date."))
+        
+        if vals.get('validity_end_date'):
+            validity_date = fields.Date.to_date(vals['validity_end_date'])
+            if validity_date < today:
+                raise ValidationError(_("Validity End Date cannot be set to a past date."))
+
+        if 'partner_id' in vals:
+            for order in self:
+                if order.state not in ('draft', 'cancel') and order.partner_id.id != vals['partner_id']:
+                    raise ValidationError(_("You cannot change the vendor after the RFQ has been sent."))
+        return super().write(vals)
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    def write(self, vals):
+        if 'product_qty' in vals:
+            for line in self:
+                if line.order_id.state not in ('draft', 'cancel'):
+                    raise ValidationError(_("You cannot change the quantity after the purchase order has been sent."))
+        return super().write(vals)
