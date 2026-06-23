73bd7b4e57bf042a746d77fb185b47bc430df9ba
Arjun <a4arjunsatheesh@gmail.com>
2026-06-23 12:41:09 +0530

    [UPDT]Added the functionality to send the email with the payment receipt when the customer makes the payment

---
+++ invoice_receipt_approval/models/account_payment.py
@@ -0,0 +1,13 @@
+from odoo import models, api
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    def action_post(self):
+        res = super(AccountPayment, self).action_post()
+        for payment in self:
+            if payment.payment_type == 'inbound' and payment.partner_type == 'customer' and payment.partner_id:
+                template = self.env.ref('invoice_receipt_approval.email_template_payment_receipt_custom', raise_if_not_found=False)
+                if template:
+                    template.send_mail(payment.id, force_send=True)
+        return res
